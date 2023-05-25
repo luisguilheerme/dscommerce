bcbfa99a7ee07b0a325ee0bff23cd700628db752
luisguilheerme <lg.maroni@gmail.com>
2023-05-24 21:02:31 -0300

    Bean validation

--- src/main/java/com/luisguilherme/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/luisguilherme/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -4,11 +4,16 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import com.luisguilherme.dscommerce.dto.CustomError;
+import com.luisguilherme.dscommerce.dto.ValidationError;
 import com.luisguilherme.dscommerce.services.exceptions.DatabaseException;
 import com.luisguilherme.dscommerce.services.exceptions.ResourceNotFoundException;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
@@ -27,5 +32,16 @@ public class ControllerExceptionHandler {
 		CustomError err = new CustomError (Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<CustomError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError err = new ValidationError (Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+		for(FieldError f : e.getBindingResult().getFieldErrors()) {
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+		return ResponseEntity.status(status).body(err);
+	}
+	
 }
 
--- src/main/java/com/luisguilherme/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/luisguilherme/dscommerce/dto/ProductDTO.java
@@ -2,11 +2,20 @@ package com.luisguilherme.dscommerce.dto;
 
 import com.luisguilherme.dscommerce.entities.Product;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public class ProductDTO {
 
 	private Long id;
+	@Size(min = 3, max = 80, message = "O nome deve ter de 3 a 80 caracteres" )
+	@NotBlank(message = "Campo Obrigatório")
 	private String name;	
+	@Size(min = 10, message = "A descrição deve ter no mínimo 10 caracteres" )
+	@NotBlank(message = "Campo Obrigatório")
 	private String description;
+	@Positive(message = "Preco deve ser positivo")
 	private Double price;
 	private String imgUrl;
 	
