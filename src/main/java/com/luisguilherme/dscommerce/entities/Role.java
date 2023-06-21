a6f397ad520c476a207dd7b8f929f4546adfbeb3
luisguilheerme <lg.maroni@gmail.com>
2023-06-21 19:15:06 -0300

    UserDetails, GrantedAuthority

--- src/main/java/com/luisguilherme/dscommerce/entities/Role.java
+++ src/main/java/com/luisguilherme/dscommerce/entities/Role.java
@@ -1,6 +1,9 @@
 package com.luisguilherme.dscommerce.entities;
 
 import java.util.Objects;
+
+import org.springframework.security.core.GrantedAuthority;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,9 +11,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 	
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,10 +38,14 @@ public class Role {
 		this.id = id;
 	}
 	
-	public Object getAuthority() {
-		// TODO Auto-generated method stub
-		return null;
+	@Override
+	public String getAuthority() {
+		return authority;
 	}	
+	
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
 
 	@Override
 	public int hashCode() {
