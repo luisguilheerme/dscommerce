781c8cd36b31ed6020582725c85d37e9f38a3168
luisguilheerme <lg.maroni@gmail.com>
2023-06-21 19:20:23 -0300

    UserDetailsService

---
+++ src/main/java/com/luisguilherme/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,10 @@
+package com.luisguilherme.dscommerce.projections;
+
+public interface UserDetailsProjection {
+
+	String getUsername();
+	String getPassword();
+	Long getRoleId();
+	String getAuthority();
+	
+}
