f36b3ea433ed370ced9054f2a8334b2c59f6cae0
luisguilheerme <lg.maroni@gmail.com>
2023-06-27 15:58:23 -0300

    Fix validation

--- src/main/java/com/luisguilherme/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/luisguilherme/dscommerce/dto/ProductDTO.java
@@ -8,6 +8,7 @@ import com.luisguilherme.dscommerce.entities.Product;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
@@ -20,6 +21,7 @@ public class ProductDTO {
 	@Size(min = 10, message = "A descrição deve ter no mínimo 10 caracteres" )
 	@NotBlank(message = "Campo Obrigatório")
 	private String description;
+	@NotNull(message = "Campo Obrigatório")
 	@Positive(message = "Preco deve ser positivo")
 	private Double price;
 	private String imgUrl;
